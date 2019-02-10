7ff7c13c25efb8a8744c563866173178e77331bc
Beth Gilomen <beth.gilomen@favordelivery.com>
2019-02-09 18:24:41 -0600

    setting up router and authorization

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import { withStyles } from '@material-ui/core/styles'
+import { Link, Route } from 'react-router-dom'
+import { Login } from './routes/login'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
   }
 }
 
-export default App;
+class App extends Component {
+  render () {
+    const { classes } = this.props
+    return (
+      <div className='App'>
+        <div className={classes.root}>
+          <AppBar position='static'>
+            <Toolbar>
+              <Typography variant='h6' color='inherit' className={classes.grow}>
+                News
+              </Typography>
+              <Link to='/login'>
+                <Button color='inherit'>Login</Button>
+              </Link>
+            </Toolbar>
+          </AppBar>
+          <div className={'container'}>
+            <Route path='/login' component={Login} />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default withStyles(styles)(App)
