91c7329e0fd55cbd0d62c8251da1318c0286670f
Beth Gilomen <beth.gilomen@favordelivery.com>
2019-02-23 17:39:48 -0600

    wip

--- src/models/user/actions.js
+++ src/models/user/actions.js
@@ -1,11 +1,15 @@
 import {
   AUTH_USER_ERROR,
   AUTH_USER_REQUEST,
-  AUTH_USER_SUCCESS, USER_SIGN_OUT_ERROR,
+  AUTH_USER_SUCCESS,
+  USER_SIGN_OUT_ERROR,
   USER_SIGN_OUT_REQUEST,
-  USER_SIGN_OUT_SUCCESS
+  USER_SIGN_OUT_SUCCESS,
+  USER_CREATE_REQUEST,
+  USER_CREATE_SUCCESS,
+  USER_CREATE_ERROR
 } from './consts'
-import { authRef, GoogleAuthProvider } from '../../firebaseConfig'
+import { authRef, GoogleAuthProvider, dbRef } from '../../firebaseConfig'
 
 export const fetchUser = () => dispatch => {
   authRef.onAuthStateChanged(user => {
@@ -14,8 +18,30 @@ export const fetchUser = () => dispatch => {
         type: AUTH_USER_SUCCESS,
         payload: user
       })
+      dispatch(createUser(user))
     }
   })
+}
+
+export const createUser = (user) => (dispatch, getState) => {
+  if (user) {
+    dispatch({
+      type: USER_CREATE_REQUEST
+    })
+    dbRef.collection('users').doc(`${user.uid}`).set({
+      displayName: user.displayName,
+      photoURL: user.photoURL
+    }).then(res => {
+      dispatch({
+        type: USER_CREATE_SUCCESS
+      })
+    }).catch(e => {
+      console.log(e)
+      dispatch({
+        type: USER_CREATE_ERROR
+      })
+    })
+  }
 }
 
 export const signIn = () => dispatch => {
@@ -49,8 +75,4 @@ export const signOut = () => dispatch => {
         error: error
       })
     })
-}
-
-export const selectUser = ({ user }) => {
-  return user.user
 }
--- src/routes/pools/actions.js
+++ src/routes/pools/actions.js
@@ -1,23 +1,37 @@
-import { dbRef } from '../../firebaseConfig'
-import { selectUser } from '../../models/user/actions'
-import { USER_POOLS_ERROR, USER_POOLS_REQUEST, USER_POOLS_SUCCESS } from './consts'
+import { dbRef, fbstore } from '../../firebaseConfig'
+import { selectUser } from '../../models/user/reducer'
+import {
+  ADD_USER_TO_POOL_ERROR,
+  ADD_USER_TO_POOL_REQUEST,
+  ADD_USER_TO_POOL_SUCCESS,
+  USER_POOLS_ERROR,
+  USER_POOLS_REQUEST,
+  USER_POOLS_SUCCESS,
+  POOL_USERS_REQUEST,
+  POOL_USERS_SUCCESS,
+  POOL_USERS_ERROR
+} from './consts'
 
 export const getPools = () => (dispatch, getState) => {
   dispatch({
     type: USER_POOLS_REQUEST
   })
-  const userId = selectUser(getState()).uid
   const collection = dbRef.collection('pools')
+  const userId = selectUser(getState()).uid
+
   if (collection) {
-    debugger
-    collection.where('users', 'array-contains', `${userId}`).get().then((data) => {
-      debugger
+    collection.where('users', 'array-contains', `${userId}`).get().then((snapshot) => {
       dispatch({
         type: USER_POOLS_SUCCESS,
-        payload: data
+        payload: snapshot.docs.map(doc => {
+          dispatch(getPoolPlayersData(doc.id, doc.data().users))
+          return {
+            id: doc.id,
+            ...doc.data()
+          }
+        })
       })
     }).catch(e => {
-      debugger
       dispatch({
         type: USER_POOLS_ERROR,
         error: e
@@ -26,4 +40,50 @@ export const getPools = () => (dispatch, getState) => {
   }
 }
 
-export const selectPools = ({ pools }) => pools.pools
+export const addUserToPool = (poolId) => (dispatch, getState) => {
+  dispatch({
+    type: ADD_USER_TO_POOL_REQUEST
+  })
+  const collection = dbRef.collection('pools')
+  const userId = selectUser(getState()).uid
+  const pool = collection.doc(`${poolId}`)
+  pool.update({
+    users: fbstore.FieldValue.arrayUnion(userId)
+  }).then((snapshot) => {
+    dispatch({
+      type: ADD_USER_TO_POOL_SUCCESS
+    })
+    dispatch(getPools())
+  }).catch(e => {
+    dispatch({
+      type: ADD_USER_TO_POOL_ERROR
+    })
+  })
+}
+
+export const getPoolPlayersData = (poolId, poolUsers) => (dispatch, getState) => {
+  const collection = dbRef.collection('users')
+  dispatch({
+    type: POOL_USERS_REQUEST
+  })
+  dbRef.runTransaction((transaction) => {
+    let poolPlayers = {}
+    poolUsers.forEach(userId => {
+      return transaction.get(collection.doc(userId)).then(user => {
+        poolPlayers[userId] = user.data()
+      })
+    })
+    return Promise.resolve(poolPlayers)
+  }).then(data => {
+    dispatch({
+      type: POOL_USERS_SUCCESS,
+      payload: data,
+      poolId
+    })
+  }).catch(e => {
+    dispatch({
+      type: POOL_USERS_ERROR,
+      error: e
+    })
+  })
+}
