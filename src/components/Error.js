9548e2bee1dbca55cc84badfe776e9e9d5681597
Beth Gilomen <beth.gilomen@favordelivery.com>
2019-03-07 23:24:50 -0600

    good progress

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,17 +1,57 @@
 import React, { Component } from 'react'
 import Modal from '@material-ui/core/Modal'
+import { withStyles } from '@material-ui/core/styles'
 
-export class Errors extends Component {
+export const CLEAR_ERRORS = 'CLEAR_ERRORS'
+
+export const clearErrors = () => (dispatch) => {
+  dispatch({
+    type: CLEAR_ERRORS
+  })
+}
+
+const styles = theme => ({
+  paper: {
+    position: 'absolute',
+    width: theme.spacing.unit * 50,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing.unit * 4,
+    outline: 'none'
+  }
+})
+
+function rand () {
+  return Math.round(Math.random() * 20) - 10
+}
+
+function getModalStyle () {
+  const top = 50 + rand()
+  const left = 50 + rand()
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`
+  }
+}
+
+class Errors extends Component {
   render () {
     const {
-      showModal,
-      handleClose,
-      errorMessage
+      error,
+      classes,
+      handleClose
     } = this.props
     return (
-      <Modal open={showModal} onClose={handleClose}>
-        <div>{errorMessage}</div>
+      <Modal open={error} onClose={handleClose}>
+        <div style={getModalStyle()} className={classes.paper}>
+          {error}
+        </div>
       </Modal>
     )
   }
 }
+const ErrorModalWrapped = withStyles(styles)(Errors)
+
+export default ErrorModalWrapped
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -3,45 +3,125 @@ import { connect } from 'react-redux'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-import Button from '@material-ui/core/Button'
+import Drawer from '@material-ui/core/Drawer'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
 import { withStyles } from '@material-ui/core/styles'
 import { signIn, signOut } from '../models/user/actions'
 import { Link } from 'react-router-dom'
+import Dragonglass from '../images/dragonglass.jpg'
 
-const styles = {
+const drawerWidth = 240
+
+const styles = theme => ({
   root: {
-    flexGrow: 1
+    display: 'flex'
   },
-  grow: {
-    flexGrow: 1
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth
   },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    background: `linear-gradient(to right, rgba(255,255,255,0) 0%,rgba(127,126,128,0.39) 59%,rgba(84,82,85,0.46) 79%,rgba(38,36,39,1)), url(${Dragonglass}) center`,
+    backgroundSize: 'cover'
+  },
+  noBorder: {
+    border: 'none'
+  },
+  toolbar: {
+    minHeight: '64px'
+  },
+  logoFont: {
+    fontFamily: 'game_of_thronesregular, serif'
+  },
+  nav: {
+    color: theme.palette.primary.contrastText,
+    textDecoration: 'none',
+    fontSize: '16px',
+    fontFamily: 'game_of_thronesregular, serif'
+  },
+  profileImage: {
+    height: 105,
+    width: 100,
+    position: 'relative',
+    zIndex: '1',
+    margin: '0 auto',
+    '-webkit-clip-path': 'polygon(51% 1%, 47% 4%, 43% 7%, 38% 9%, 34% 11%, 28% 12%, 23% 11%, 19% 11%, 14% 10%, 9% 8%, 8% 14%, 8% 20%, 8% 26%, 8% 32%, 8% 39%, 9% 45%, 10% 50%, 12% 56%, 14% 62%, 16% 68%, 20% 73%, 24% 79%, 28% 84%, 32% 88%, 37% 92%, 41% 95%, 46% 97%, 50% 99%, 55% 97%, 59% 95%, 64% 92%, 68% 88%, 73% 85%, 76% 82%, 79% 77%, 83% 72%, 86% 68%, 88% 62%, 90% 57%, 91% 51%, 92% 46%, 92% 41%, 93% 36%, 93% 31%, 93% 27%, 93% 22%, 93% 18%, 93% 14%, 92% 8%, 87% 10%, 82% 11%, 77% 12%, 72% 12%, 67% 11%, 63% 9%, 59% 7%, 56% 4%)',
+    clipPath: `polygon(51% 1%, 47% 4%, 43% 7%, 38% 9%, 34% 11%, 28% 12%, 23% 11%, 19% 11%, 14% 10%, 9% 8%, 8% 14%, 8% 20%, 8% 26%, 8% 32%, 8% 39%, 9% 45%, 10% 50%, 12% 56%, 14% 62%, 16% 68%, 20% 73%, 24% 79%, 28% 84%, 32% 88%, 37% 92%, 41% 95%, 46% 97%, 50% 99%, 55% 97%, 59% 95%, 64% 92%, 68% 88%, 73% 85%, 76% 82%, 79% 77%, 83% 72%, 86% 68%, 88% 62%, 90% 57%, 91% 51%, 92% 46%, 92% 41%, 93% 36%, 93% 31%, 93% 27%, 93% 22%, 93% 18%, 93% 14%, 92% 8%, 87% 10%, 82% 11%, 77% 12%, 72% 12%, 67% 11%, 63% 9%, 59% 7%, 56% 4%)`
   }
-}
+})
 
 class TopBar extends Component {
   render () {
     const { classes, signIn, signOut, user } = this.props
     return (
-      <div>
-        <AppBar position='static'>
+      [
+        <AppBar key={'app-bar'} position='fixed' className={classes.appBar}>
           <Toolbar>
-            <Typography variant='h6' color='inherit' className={classes.grow}>
+            <Typography variant='h6' color='inherit' className={classes.logoFont}>
                 Valar Morghulis
             </Typography>
-            {user && (
-              <div>
-                <Link to={'/bracket'}><Button color='inherit'>Your Bracket</Button></Link>
-                <Link to={'/pools'}><Button color='inherit'>Pools</Button></Link>
-                <Button color='inherit' onClick={signOut}>Log Out</Button>
-              </div>
-            )}
-            {!user && <Button color='inherit' onClick={signIn}>Login</Button>}
           </Toolbar>
-        </AppBar>
-      </div>
+        </AppBar>,
+        <Drawer key={'drawer'}
+          className={classes.drawer}
+          variant='permanent'
+          classes={{
+            paper: classes.drawerPaper,
+            paperAnchorDockedLeft: classes.noBorder
+          }}
+          anchor='left'
+        >
+          <div className={classes.toolbar} />
+          {user && (
+            <div>
+              <div style={{ filter: 'drop-shadow(#fff 0px 0px 2px)' }}>
+                <div style={{
+                  background: `url(${user.photoURL}) center no-repeat`,
+                  backgroundSize: 'cover'
+                }} className={classes.profileImage} />
+              </div>
+              <List>
+                <Link to={'/'} className={classes.nav}>
+                  <ListItem button key={'Rules'}>
+                    <ListItemText classes={{ primary: classes.nav }} primary={'Rules'}>Rules</ListItemText>
+                  </ListItem>
+                </Link>
+                <Link to={'/bracket'} className={classes.nav}>
+                  <ListItem button key={'Your Bracket'}>
+                    <ListItemText classes={{ primary: classes.nav }} primary={'Your Bracket'} />
+                  </ListItem>
+                </Link>
+                <Link to={'/pools'} className={classes.nav}>
+                  <ListItem button key={'Pools'}>
+                    <ListItemText classes={{ primary: classes.nav }} primary={'Pools'} />
+                  </ListItem>
+                </Link>
+                <ListItem onClick={signOut} button key={'Log Out'} className={classes.nav}>
+                  <ListItemText classes={{ primary: classes.nav }} primary={'Log Out'} />
+                </ListItem>
+              </List>
+            </div>
+          )}
+          {!user &&
+            <List>
+              <Link to={'/'} className={classes.nav}>
+                <ListItem button key={'Rules'}>
+                  <ListItemText classes={{ primary: classes.nav }} primary={'Rules'} />
+                </ListItem>
+              </Link>
+              <ListItem onClick={signIn} button key={'Sign In'} className={classes.nav}>
+                <ListItemText classes={{ primary: classes.nav }} primary={'Sign In / Sign Up'} />
+              </ListItem>
+            </List>}
+        </Drawer>
+      ]
     )
   }
 }
