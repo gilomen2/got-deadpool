969bbd6495afb0d05e23fa0b27006a7e3625f8f7
Beth Gilomen <beth.gilomen@favordelivery.com>
2019-03-09 21:25:11 -0600

    may firebase functions deployable

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -5,41 +5,44 @@ admin.initializeApp()
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.scorePools = functions.https.onRequest(async (request, response) => {
-  admin.firestore().collection('game').doc('test1').get().then(gameSnapshot => {
+exports.scorePools = functions.https.onRequest(async function (request, response) {
+  admin.firestore().collection('game').doc('master').get().then(function (gameSnapshot) {
     let game = gameSnapshot.data()
 
-    admin.firestore().collection('test-characters').get().then(charactersSnapshot => {
+    admin.firestore().collection('characters').get().then(function (charactersSnapshot) {
       let characterData = {}
       charactersSnapshot.docs.forEach(character => {
         characterData[character.id] = character.data()
       })
 
-      admin.firestore().collection('users').get().then(usersSnapshot => {
+      admin.firestore().collection('users').get().then(function (usersSnapshot) {
         let users = {}
-        usersSnapshot.docs.forEach(user => {
+        usersSnapshot.docs.forEach(function (user) {
           const userData = user.data()
           users[user.id] = userData
           users[user.id].score = scoreBracket(userData.bracket, game, characterData)
         })
 
-        admin.firestore().collection('pools').get().then(poolsSnapshot => {
-          let promises = poolsSnapshot.docs.map(pool => {
+        admin.firestore().collection('pools').get().then(function (poolsSnapshot) {
+          let promises = poolsSnapshot.docs.map(function (pool) {
             const poolData = pool.data()
-            const poolPlayers = poolData.users ? poolData.users.map(userId => {
+            const poolPlayers = poolData.users ? poolData.users.map(function (userId) {
               return users[userId]
             }) : []
-            return admin.firestore().collection('pools').doc(pool.id).set({ players: poolPlayers }, { merge: true })
+            return admin.firestore().collection('pools').doc(pool.id).set({ players: poolPlayers }, { merge: true }).then(function (res) {
+              return res
+            }).catch(function (e) {
+              return e
+            })
           })
 
-          Promise.all(promises).then(res => {
+          Promise.all(promises).then(function (res) {
             response.send(JSON.stringify({
               game,
               users,
               characterData
             }, null, 3))
-          }).catch(e => {
-            console.log(e)
+          }).catch(function (e) {
             response.send(e)
           })
         })
@@ -52,7 +55,7 @@ function scoreBracket (bracket, game, characterData) {
   const { scoring: gameScoring } = game
   let score = 0
   if (bracket) {
-    Object.keys(bracket).forEach(characterName => {
+    Object.keys(bracket).forEach(function (characterName) {
       let characterStatus = characterData[characterName]
 
       let characterPrediction = bracket[characterName]
@@ -83,20 +86,22 @@ function scoreBracket (bracket, game, characterData) {
   return score
 }
 
-exports.copyPools = functions.https.onRequest(async (request, response) => {
+exports.copyPools = functions.https.onRequest(async function (request, response) {
   admin.firestore().collection('pools').get().then(poolsSnapshot => {
     let copyOfPoolsData = {}
-    poolsSnapshot.docs.forEach(pool => {
+    poolsSnapshot.docs.forEach(function (pool) {
       copyOfPoolsData[pool.id] = pool.data()
     })
 
-    const promises = Object.keys(copyOfPoolsData).map(poolId => {
+    const promises = Object.keys(copyOfPoolsData).map(function (poolId) {
       console.log(poolId)
       return admin.firestore().collection('test-pools').add(copyOfPoolsData[poolId])
     })
 
-    Promise.all(promises).then(ps => {
-      response.send('OK')
+    Promise.all(promises).then(function (ps) {
+      return response.send('OK')
+    }).catch(function (e) {
+      return response.send('Nope')
     })
   })
 })
