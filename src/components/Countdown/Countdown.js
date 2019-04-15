56e1dd46e2ea251305a5ba8fb3968909dfff7b1c
Beth Gilomen <beth.gilomen@favordelivery.com>
2019-04-14 21:09:59 -0500

    add week 1 scores

--- src/components/Countdown/Countdown.js
+++ src/components/Countdown/Countdown.js
@@ -1,4 +1,3 @@
-import { pad2 } from '../../utils/pad2'
 import React from 'react'
 import Countdown from 'react-countdown-now'
 import './Countdown.scss'
@@ -6,21 +5,15 @@ import './Countdown.scss'
 const Completionist = () => <span />
 
 const CountdownRender = ({ days, hours, minutes, seconds, completed }) => {
-  if (completed) {
-    // Render a completed state
-    return <Completionist />
-  } else {
-    // Render a countdown
-    return (
-      <div className={'countdown-container'}>
-        <span key={'counter'} className={'countdown'}>{pad2(days)}:{pad2(hours)}:{pad2(minutes)}:{pad2(seconds)}</span>
-      </div>
-    )
-  }
+  return (
+    <div className={'countdown-container'}>
+        Week 1 Scores Available Now!
+    </div>
+  )
 }
 
 export const CountdownTimer = () => (
-  <Countdown renderer={CountdownRender} zeroPadTime={2} date={new Date('2019-04-14 20:00:00')}>
-    <Completionist />
-  </Countdown>
+  <div className={'countdown-container'}>
+    Week 1 Scores Available Now!
+  </div>
 )
